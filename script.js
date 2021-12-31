91efede53864623e739fe114ba5a4b1ee19e16aa
Norman Monteiro <montein56@hotmail.com>
2021-12-31 00:04:36 -0500

    tried a few things... long way to go

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 console.log('Happy New year');
 
-let arr = ['Rock', 'Paper', 'Scissors'];
+const arr = ['ROCK', 'PAPER', 'SCISSORS'];
 
 function computerPlay() {
-    computerSelection = arr[Math.floor(Math.random() * arr.length)];
-    return computerSelection;
+    return arr[Math.floor(Math.random() * arr.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    result = "";
+    if (playerSelection == computerSelection) {
+        result = 'TIE GAME';
+        return result;
+    } else {
+        result = 'Somebody won and someone else lost';
+        return result;
+    }
+}
+// const computerSelection = () => arr[Math.floor(Math.random() * arr.length)];
+const computerSelection = computerPlay();
+console.log(`Computer chose ${computerSelection}`);
+
+const playerSelection = (window.prompt("Select your weapon", "Rock - Paper - Scissors")).toUpperCase();
+console.log(`You selected ${playerSelection}`);
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+// function computerPlay() {
+//     computerSelection = arr[Math.floor(Math.random() * arr.length)];
+//     return computerSelection;
+// }
